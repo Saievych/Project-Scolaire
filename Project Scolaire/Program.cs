2ba52bc4f7426c28af72cdc835a7431984ade8da
Saievych <saevich.is@gmail.com>
2017-12-08 11:55:46 +0100

    Version 2 : Multithread Reading
    Parallel.ForEach

--- Project Scolaire/Program.cs
+++ Project Scolaire/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Diagnostics;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -14,7 +13,7 @@ namespace FileCreater
         const int blockSize = 1024 * 8;
         const int blocksPerMb = (1024 * 1024) / blockSize;
         const string legalCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789 \n";
-
+        const string searchPhrase = "gdfgdfHDF";
         private static Random random = new Random();
         private static string fileName = System.IO.Directory.GetParent(@"../../ ").FullName + "/text.txt";
 
@@ -23,42 +22,79 @@ namespace FileCreater
             return new string(Enumerable.Repeat(legalCharacters, blockSize)
               .Select(s => s[random.Next(s.Length)]).ToArray());
         }
-        static void Main(string[] args)
+
+        static void GenerateFile()
         {
-            Stopwatch watch = Stopwatch.StartNew();
             string test = RandomString();
-            //byte[] data = new byte[blockSize];
             byte[] data = Encoding.ASCII.GetBytes(RandomString());
             using (FileStream stream = File.OpenWrite(fileName))
             {
                 for (int i = 0; i < sizeInMb * blocksPerMb; i++)
                 {
-                    //random.NextBytes(data);
                     stream.Write(data, 0, data.Length);
                 }
             }
-            watch.Stop();
-            long timetest = watch.ElapsedMilliseconds;
-            watch.Restart();
-            IEnumerable<string> lines = File.ReadAllLines(fileName);
-            //int numb = random.Next(lines.Count());
-            string rndphrase = lines.ElementAt(random.Next(lines.Count()));
-            Console.Write("Your phrase to search is {0}", rndphrase);
-            //We identify the matches. If the input is empty, then we return no matches at all
-            IEnumerable<string> matches = !String.IsNullOrEmpty(rndphrase)
-                                          ? lines.Where(line => line.IndexOf(rndphrase, StringComparison.OrdinalIgnoreCase) >= 0)
-                                          : Enumerable.Empty<string>();
+        }
 
-            //Console.WriteLine(matches.Any()
-            //                  ? String.Format("Matches:\n> {0}", String.Join("\n> ", matches))
-            //                  : "There were no matches");
-            watch.Stop();
-            //timetest = watch.ElapsedMilliseconds;
-            Console.WriteLine("Your time for creating file: {0}; {1} And for searching your phrase : {2}", timetest, System.Environment.NewLine, watch.ElapsedMilliseconds);
-            //foreach (var line in lines)
-            //{
-            //   Console.WriteLine(line);
-            //}
+        static void Main(string[] args)
+        {
+            if (!File.Exists(fileName))
+            {
+                GenerateFile();
+            }
+            for (int i = 0; i < 25; i++)
+            {
+                Stopwatch watch = System.Diagnostics.Stopwatch.StartNew();
+                bool found = false;
+                
+                watch.Start();
+                Parallel.ForEach(File.ReadLines(fileName).AsParallel(), new ParallelOptions() { MaxDegreeOfParallelism = Environment.ProcessorCount }, (line, state) =>
+                {
+                    if (line.Contains(searchPhrase))
+                    {
+                        found = true;
+                        state.Stop();
+                        return;
+                    }
+                });
+
+                if (found)
+                {
+                    Console.WriteLine("Your time for searching your phrase ({0}) : {1}", searchPhrase, watch.ElapsedMilliseconds);
+                }
+                else
+                {
+                    Console.WriteLine("There is no your phrase in file, total time is : {0}", watch.ElapsedMilliseconds);
+                }
+                watch.Stop();
+            }
+
+            for (int i = 0; i < 25; i++)
+            {
+                Stopwatch watch = System.Diagnostics.Stopwatch.StartNew();
+                bool found = false;
+
+                watch.Start();
+                Parallel.ForEach(File.ReadLines(fileName), new ParallelOptions() { MaxDegreeOfParallelism = Environment.ProcessorCount }, (line, state) =>
+                {
+                    if (line.Contains(searchPhrase))
+                    {
+                        found = true;
+                        state.Stop();
+                        return;
+                    }
+                });
+
+                if (found)
+                {
+                    Console.WriteLine("Your time for searching your phrase ({0}) : {1}", searchPhrase, watch.ElapsedMilliseconds);
+                }
+                else
+                {
+                    Console.WriteLine("There is no your phrase in file, total time is : {0}", watch.ElapsedMilliseconds);
+                }
+                watch.Stop();
+            }
             Console.ReadLine();
         }
     }
