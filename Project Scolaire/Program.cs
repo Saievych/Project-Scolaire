a2395766aeefa508edabec569f750a117b1f10ae
Saievych <saevich.is@gmail.com>
2017-12-20 22:47:13 +0100

    Final results with byte array
    Reading file like bytearray and searching phrase with matching two arrays : data and phrase

--- Project Scolaire/Program.cs
+++ Project Scolaire/Program.cs
@@ -35,19 +35,66 @@ namespace FileCreater
             }
         }
 
+        static int search(byte[] data, byte[] needle)
+        {
+            for (int i = 0; i <= data.Length - needle.Length; i++)
+            {
+                if (match(data, needle, i))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        static bool match(byte[] data, byte[] needle, int start)
+        {
+            if (needle.Length + start > data.Length)
+            {
+                return false;
+            }
+            else
+            {
+                for (int i = 0; i < needle.Length; i++)
+                {
+                    if (needle[i] != data[i + start])
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+
         static void Main(string[] args)
         {
             if (!File.Exists(fileName))
             {
                 GenerateFile();
             }
-           
+            Stopwatch watch = Stopwatch.StartNew();
+            watch.Start();
+            byte[] toBytes = Encoding.ASCII.GetBytes("7iSSNF");
+           // byte[] toBytes2 = Encoding.ASCII.GetBytes("Us6hCWhN2hrd93PzyxRZYijT2PmncHQyG3ckcbXLWImx87JcqTuu8RlLimrYK5KtB4HfZFxwKgcp8XW6gk8unvCZRCb1U");
+            var data = File.ReadAllBytes(fileName);
+            var foundIndex = search(data, toBytes);
+            if (foundIndex > -1)
+            {
+                var resArr = new byte[toBytes.Length];
+                for (int i = 0; i < toBytes.Length; i++)
+                {
+                    resArr[i] = data[i + foundIndex];
+                }
+                string result = Encoding.ASCII.GetString(resArr);
+
+                Console.WriteLine("Your time for searching your phrase ({0}) : {1}, ticks : {2}", result, watch.Elapsed.Seconds, watch.Elapsed.Ticks);
+            }
+            watch.Stop();
+            watch.Reset();
             Console.WriteLine("5 tests for multithread methode : Parallel.ForEach(File.ReadLines(fileName).AsParallel()");
             for (int i = 0; i < 5; i++)
             {
-                Stopwatch watch = Stopwatch.StartNew();
                 bool found = false;
-
                 watch.Start();
                 Parallel.ForEach(File.ReadLines(fileName).AsParallel(), new ParallelOptions() { MaxDegreeOfParallelism = Environment.ProcessorCount }, (line, state) =>
                 {
@@ -68,12 +115,12 @@ namespace FileCreater
                     Console.WriteLine("There is no your phrase in file, total time is : {0}, ticks : {1}", watch.Elapsed.Seconds, watch.Elapsed.Ticks);
                 }
                 watch.Stop();
+                watch.Reset();
             }
 
             Console.WriteLine("5 tests for multithread methode : Parallel.ForEach(File.ReadLines(fileName)");
             for (int i = 0; i < 5; i++)
             {
-                Stopwatch watch = Stopwatch.StartNew();
                 bool found = false;
 
                 watch.Start();
@@ -96,12 +143,12 @@ namespace FileCreater
                     Console.WriteLine("There is no your phrase in file, total time is : {0}, ticks : {1}", watch.Elapsed.Seconds, watch.Elapsed.Ticks);
                 }
                 watch.Stop();
+                watch.Reset();
             }
 
             Console.WriteLine("Test for one-thread methode : File.ReadLines(fileName)");
             using (MemoryStream stream = new MemoryStream())
             {
-                Stopwatch watch = Stopwatch.StartNew();
                 watch.Start();
                 var lines = File.ReadAllLines(fileName);
                 if (lines.Contains(searchPhrase))
@@ -114,6 +161,7 @@ namespace FileCreater
                 }
                 lines = null;
                 watch.Stop();
+                watch.Reset();
             }
             Console.ReadLine();
         }
